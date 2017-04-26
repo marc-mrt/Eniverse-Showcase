1a388365bc61df6d63d41e671d34affffd1d71c3
Marc Morant <marc.morant@epitech.eu>
2017-04-26 17:38:25 +0200

    App skeleton

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import { sync } from 'vuex-router-sync';
+
+import Vue from 'vue';
+import App from './App';
+import store from './store';
+import router from './router';
+
+Vue.config.productionTip = false;
+
+sync(store, router);
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App },
+});
