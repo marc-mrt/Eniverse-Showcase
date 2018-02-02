31461024b603fe0a942c0cdbd6b7b2c42b275117
Marc Morant <marc.morant@epitech.eu>
2018-02-02 00:03:14 +0000

    Fixed a molecule in Partners

--- src/components/pages/Partners.jsx
+++ src/components/pages/Partners.jsx
@@ -3,8 +3,7 @@
 import React from 'react';
 import PageSection from '../templates/PageSection';
 import * as content from '../../contents/Partners';
-import PartnersListContainer from '../atoms/PartnersListContainer';
-import PartnersListPartner from '../molecules/PartnersListPartner';
+import PartnersList from '../molecules/PartnersList';
 import interbackLight from '../../assets/images/interback-light.png';
 
 const Partners = () => (
@@ -14,13 +13,7 @@ const Partners = () => (
     background={interbackLight}
     hasPadding
   >
-    <PartnersListContainer className="columns is-multiline is-centered">
-      {content.partners.map(partner => (
-        <div className="column is-1" key={partner.img.toString()}>
-          <PartnersListPartner partner={partner} />
-        </div>
-      ))}
-    </PartnersListContainer>
+    <PartnersList partners={content.partners} />
   </PageSection>
 );
 
