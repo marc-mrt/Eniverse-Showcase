0f5369c7107577b561646f6381e29d08382a0f98
Marc Morant <marc.morant@epitech.eu>
2018-02-05 19:29:49 +0000

    Added video background possibility

--- src/components/pages/Hero.jsx
+++ src/components/pages/Hero.jsx
@@ -3,9 +3,11 @@
 import React from 'react';
 import PageSection from '../templates/PageSection';
 import HeroBackground from '../atoms/HeroBackground';
+import VideoBackground from '../atoms/VideoBackground';
 import HeroMainTitle from '../molecules/HeroMainTitle';
 import * as content from '../../contents/Hero';
 import logo from '../../assets/logo.png';
+import * as store from '../../store';
 
 const Hero = () => (
   <PageSection id="hero">
@@ -13,6 +15,13 @@ const Hero = () => (
       <div className="hero-body">
         <HeroMainTitle logo={logo} content={content} />
       </div>
+      {store.getDesktop() ? (
+        <VideoBackground id="video-background" muted playsInline loop autoPlay>
+          <source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4" />
+        </VideoBackground>
+      ) : (
+        false
+      )}
     </HeroBackground>
   </PageSection>
 );
